8256ab54e35d4a5b83e057a4ab97b9c81b18f145
Manya Malhotra <malhotra.manyaaaa@gmail.com>
2025-01-10 01:34:08 +0000

    Initial Commit

---
+++ equicare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  equicare
+//
+//  Created by Manya Malhotra on 10/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ equicare/equicareApp.swift
@@ -0,0 +1,17 @@
+//
+//  equicareApp.swift
+//  equicare
+//
+//  Created by Manya Malhotra on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct equicareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
